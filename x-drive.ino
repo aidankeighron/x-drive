a5ba52a83493413a73deddf2a13910c741888094
aidankeighron <aidankeighron@gmail.com>
2023-10-04 17:15:12 -0400

    1 motor

---
+++ x-drive.ino
@@ -0,0 +1,57 @@
+#include <AFMotor.h>
+
+AF_DCMotor FR_Motor(1);
+AF_DCMotor FL_Motor(2);
+AF_DCMotor BR_Motor(3);
+AF_DCMotor BL_Motor(4);
+
+void setup() {
+  Serial.begin(9600);
+
+  Serial.println("DRV8871 test");
+  
+  BR_Motor.setSpeed(100);
+  BR_Motor.run(RELEASE);
+}
+
+void loop() 
+{
+  uint8_t i;
+  
+//Motor spinning clockwise
+  BR_Motor.run(FORWARD);
+  //Speed up
+  for (i=0; i<255; i++) 
+  {
+    BR_Motor.setSpeed(i);  
+    delay(10);
+  }
+  
+  // Speed down
+  for (i=255; i!=0; i--) 
+  {
+    BR_Motor.setSpeed(i);  
+    delay(10);
+  }
+
+//Motor spinning anti-clockwise
+  BR_Motor.run(BACKWARD);
+  
+  // Speed up
+  for (i=0; i<255; i++) 
+  {
+    BR_Motor.setSpeed(i);  
+    delay(10);
+  }
+
+  // Speed down
+  for (i=255; i!=0; i--) 
+  {
+    BR_Motor.setSpeed(i);  
+    delay(10);
+  }
+
+  // Now turn off motor
+  BR_Motor.run(RELEASE);
+  delay(1000);
+}
