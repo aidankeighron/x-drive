bd929162dd3c33782ea0cb8a211ada413515be21
aidankeighron <aidankeighron@gmail.com>
2023-10-09 00:10:51 -0400

    more motors

--- x-drive.ino
+++ x-drive.ino
@@ -19,39 +19,60 @@ void loop()
   uint8_t i;
   
 //Motor spinning clockwise
+  FR_Motor.run(FORWARD);
+  FL_Motor.run(FORWARD);
   BR_Motor.run(FORWARD);
+  BL_Motor.run(FORWARD);
   //Speed up
   for (i=0; i<255; i++) 
   {
+    FR_Motor.setSpeed(i);  
+    FL_Motor.setSpeed(i);  
     BR_Motor.setSpeed(i);  
+    BL_Motor.setSpeed(i);  
     delay(10);
   }
   
   // Speed down
   for (i=255; i!=0; i--) 
   {
+    FR_Motor.setSpeed(i);  
+    FL_Motor.setSpeed(i);  
     BR_Motor.setSpeed(i);  
+    BL_Motor.setSpeed(i); 
     delay(10);
   }
 
 //Motor spinning anti-clockwise
+  FR_Motor.run(BACKWARD);
+  FL_Motor.run(BACKWARD);
   BR_Motor.run(BACKWARD);
+  BL_Motor.run(BACKWARD);
   
   // Speed up
   for (i=0; i<255; i++) 
   {
+    FR_Motor.setSpeed(i);  
+    FL_Motor.setSpeed(i);  
     BR_Motor.setSpeed(i);  
+    BL_Motor.setSpeed(i);
     delay(10);
   }
 
   // Speed down
   for (i=255; i!=0; i--) 
   {
+    FR_Motor.setSpeed(i);  
+    FL_Motor.setSpeed(i);  
     BR_Motor.setSpeed(i);  
+    BL_Motor.setSpeed(i);
     delay(10);
   }
 
   // Now turn off motor
+  FR_Motor.run(RELEASE);
+  FL_Motor.run(RELEASE);
+  BR_Motor.run(RELEASE);
   BR_Motor.run(RELEASE);
   delay(1000);
 }
